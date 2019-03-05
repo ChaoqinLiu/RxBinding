a40fc2d9be864c5413458ce9dd923262d3ba1b38
ChaoqinLiu <1026191382@qq.com>
2019-03-05 16:13:11 +0800

    first commit

---
+++ app/src/main/java/com/rxbinding/trantor/rxbinding/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rxbinding.trantor.rxbinding;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
