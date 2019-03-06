1f3c4859c2196bff5fc2d7f6d59a93f5ec8e200c
ChaoqinLiu <1026191382@qq.com>
2019-03-06 10:23:57 +0800

    commit update

--- app/src/main/java/com/rxbinding/trantor/rxbinding/MainActivity.java
+++ app/src/main/java/com/rxbinding/trantor/rxbinding/MainActivity.java
@@ -2,6 +2,13 @@ package com.rxbinding.trantor.rxbinding;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.jakewharton.rxbinding2.view.RxView;
+
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.btn_click);
+        RxView.clicks(button)
+                .subscribe(new Consumer<Object>() {
+                    @Override
+                    public void accept(@NonNull Object o) throws Exception{
+                        Toast.makeText(MainActivity.this,"演示点击事件",Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        Button btn_long = (Button) findViewById(R.id.btn_longClick);
+        RxView.longClicks(btn_long)
+                .subscribe(new Consumer<Object>() {
+                    @Override
+                    public void accept(@NonNull Object o) throws Exception{
+                        Toast.makeText(MainActivity.this,"演示长击时间",Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
